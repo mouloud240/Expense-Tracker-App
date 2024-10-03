d97fa8b782dc24ca233785e273327e4b0c05cc6e
Mouloud Hasrane <m.hasrane@esi-sba.dz>
2024-10-03 10:00:33 +0100

    almost finished expenses routes

--- Backend/routes/expenses/expenses.js
+++ Backend/routes/expenses/expenses.js
@@ -1,4 +1,5 @@
 const expsenseRouter = require('express').Router();
+const { Router } = require('express');
 const User=require('../../Schemas/userShcema')
 expsenseRouter.post('/addExpense', async (req, res) => {
   const jwt=req.body.jwt;
@@ -6,7 +7,7 @@ expsenseRouter.post('/addExpense', async (req, res) => {
     res.send('provide jwt');
     return
   }
-  const user = await User.findOne({Name:"Mouloud24"});
+  const user = await User.findOne({Jwt:jwt});
   if (!user){
     res.send('user not found!');
 
@@ -18,22 +19,22 @@ expsenseRouter.post('/addExpense', async (req, res) => {
   }
      
   const total=user.totalBalance;
-console.log(req.body)
- User.updateOne({Name:"Mouloud24"},{totalBalance:total-req.body.Expense.Amount.Amount})
- const added=User.updateOne({Name:"Mouloud24"},{$push:{Expenses:req.body.Expense}});
-  console.log(added)
-  res.send('Added Expense')
+ await User.updateOne({Jwt:jwt},{$set:{ totalBalance:total-req.body.Expense.Amount.Amount }})
+ await User.updateOne({Jwt:jwt},{$push:{Expenses:req.body.Expense}});
+  const newUser=await User.findOne({Jwt:jwt})
+  res.json({
+    status:"Added an expenses",
+    newBalance:newUser.totalBalance
+  })
 })
 expsenseRouter.get('/Expenses/:jwt',async (req,res)=>{
   const jwt=req.params.jwt.trim();
   if (!jwt){
     res.send('Provide a token')
   }
-  console.log(jwt)
   
 
 const user = await User.findOne({ Jwt: jwt });
-  console.log(user)
   if(!user){
     res.send('user not found!');
     return;
@@ -42,18 +43,104 @@ const user = await User.findOne({ Jwt: jwt });
 })
 expsenseRouter.get('/ExpensesByCat/:jwt',async (req,res)=>{
   const category=req.query.category;
+  const jwt=req.params.jwt;
+  if (!jwt){
+    res.send('Provide a token')
+    return;
+  }
   if (!category){
     res.send('No Category')
    return;
   }
-  const user=await User.findOne({Jwt:req.params.jwt});
+  const user=await User.findOne({Jwt:jwt});
   if (!user){
     res.send('Wrong Token');
     return;
   }
-  const tasks=user.Expenses.find({Category:category});
-  res.json({"expenses":tasks});
+  const expenses=user.Expenses.find(exp=>exp.Category==category);
+  res.json({"expenses":expenses});
     
   })
+expsenseRouter.delete("/Expense/:jwt", async(req,res)=>{
+  const id=req.query.id;
+  const jwt=req.params.jwt;
+  if (!id){
+    res.send('Provide an id')
+  }
+  if (!jwt){
+    res.send('Provide a Valid Token')
+  }
+  let user=await User.findOne({Jwt:jwt})
+  if (!user){
+    res.send('User Not found');
+    return;
+  }
+  const expenseAmount=user.Expenses.find(exp=>exp._id.toString()==id).Amount.Amount
+  if (!expenseAmount){
+    res.send('Expense Error') //remove Later
+    return;
+  }
+  try {
+     
+      user.Expenses = user.Expenses.filter(expense => expense._id.toString() !== id);
+    user.totalBalance+=expenseAmount
+    await user.save()
+    res.json({
+      status:"Delete expense",
+      newAmount:user.totalBalance
+    })
+       
+  } catch (error) {
+
+    res.status(500).send('Failed to delete Expense')
+  }
+
+
+})
+expsenseRouter.get('/Expense/:jwt',async(req,res)=>{
+  const jwt=req.params.jwt;
+  const id=req.query.id;
+  if (!id){
+    res.send('Provide an id')
+  }
+  if (!jwt){
+    res.send('Provide a Valid Token')
+  }
+  const user=await User.findOne({Jwt:jwt})
+  if (!user){
+    res.send('User not found')
+    return;
+  }
+  const expense=user.Expenses.find(exp=>exp._id==id)
+  if (!expense){
+    res.send('Expense not Found')
+    return;
+  }
+  res.json(expense)
+  
+})
+expsenseRouter.delete('/ExpensesAll/:jwt',async (req,res)=>{
+  const jwt=req.params.jwt;
+  if (!jwt){
+    res.send('Provide a jwt')
+    return;
+  }
+  const user=await User.findOne({Jwt:jwt})
+
+  if (!user){
+    res.send('User not found');
+    return;
+  }
+  let total=0 
+  user.Expenses.map((item)=>{total+=item.Amount.Amount},)
+  console.log(total)
+  user.totalBalance+=total
+user.Expenses=[]
+await user.save()
+res.json({
+    status:"Deleted All",
+    newBalace:user.totalBalance
+  })
+})
 
 module.exports=expsenseRouter;
